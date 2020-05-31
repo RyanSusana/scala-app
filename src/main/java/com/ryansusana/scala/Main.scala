56b9b8a6352b947d6b19fb80088654d66e5e053f
Ryan Susana <ryansusana@live.com>
2020-05-31 16:37:04 +0200

    return filenames

--- src/main/java/com/ryansusana/scala/Main.scala
+++ src/main/java/com/ryansusana/scala/Main.scala
@@ -1,11 +1,17 @@
 package com.ryansusana.scala
 
+import java.util.stream.Collectors
+
 import com.google.cloud.functions.{HttpFunction, HttpRequest, HttpResponse}
 
 class Main extends HttpFunction {
   def service(request: HttpRequest, response: HttpResponse): Unit = {
 
+    // Request.getParts is a Map<String, Part>
+    val filenames = request.getParts.entrySet()
+      .stream() map { e => e.getKey } collect Collectors.joining(",")
+
     val writer = response.getWriter
-    writer.write("Hello World, from Ryan!")
+    writer.write(s"$filenames")
   }
 }
