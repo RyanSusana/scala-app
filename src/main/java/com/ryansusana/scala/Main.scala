f4301eb94a54c17f57c836bafc6155905c1c83d0
Ryan Susana <ryansusana@live.com>
2020-05-31 17:18:59 +0200

    added content type  check

--- src/main/java/com/ryansusana/scala/Main.scala
+++ src/main/java/com/ryansusana/scala/Main.scala
@@ -8,8 +8,14 @@ class Main extends HttpFunction {
   def service(request: HttpRequest, response: HttpResponse): Unit = {
 
     // Request.getParts is a Map<String, Part>
-    val filenames = request.getParts.entrySet()
-      .stream() map { e => e.getKey } collect Collectors.joining(",")
+
+    val contentType = request.getContentType.orElse("application/json")
+    val filenames =
+      if (contentType contains "multipart")
+        request.getParts.entrySet().stream()
+          .map { e => e.getKey } collect Collectors.joining(",")
+      else
+        "none"
 
     val writer = response.getWriter
     writer.write(s"Files: $filenames")
